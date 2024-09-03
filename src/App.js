613a455da27a2bd4f55c514fdd3577f5d240565a
arunkumar404 <rarun1395@gmail.com>
2024-09-04 02:37:10 +0530

    test 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import CustomTabs from './CustomTabs/CustomTabs';
 import Holdings from './Holdings/Header';
+import Positions from './Positions/Positions';
 
 function App() {
   return (
     <div className='app'>
       <Holdings/>
       <CustomTabs />
+      <Positions/>
+      
     </div>
   );
 }
---
+++ src/Positions/Positions.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import './Positions.css'
+
+const Positions = () => {
+    const [totalPL, setTotalPL] = useState('4,091.25')
+  return (
+    <div className="total_pnl_container">
+      <div className="pnlContainer">
+        <div className="pnlSingle">
+          <p className="plType remove_pm">Total P&L</p>
+          <p className="plAmount remove_pm">₹{totalPL}</p>
+        </div>
+      </div>
+      <div className="pnl_Bottom">
+        <div className="pnlContainer">
+          <div className="pnlSingle">
+            <p className="plType remove_pm">Total P&L</p>
+            <p className="plAmount remove_pm">₹{totalPL}</p>
+          </div>
+        </div>
+        <div className='vertical_bar'/>
+        <div className="pnlContainer">
+          <div className="pnlSingle">
+            <p className="plType remove_pm">Total P&L</p>
+            <p className="plAmount remove_pm">₹{totalPL}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Positions
